0ff44bd6ac360cf713890635189952654950c3b0
rvsrafael <rvs.contato@gmail.com>
2017-12-10 21:23:52 -0200

    request games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EighthGamesProvider } from '../providers/eighth-games/eighth-games';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    EighthGamesProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EighthGamesProvider } from '../../providers/eighth-games/eighth-games';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public eighthGames: any;
 
+  constructor(public navCtrl: NavController,
+              public eighthGamesProvider: EighthGamesProvider
+) {
+      this.loadGames();
+  }
+
+  loadGames(){
+      this.eighthGamesProvider.load()
+      .then(data => {
+        this.eighthGames = data;
+      });
   }
 
 }
