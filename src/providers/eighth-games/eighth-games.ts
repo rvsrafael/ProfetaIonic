5bad5a49619deb995726a7f358fae84dfd54f85e
rvsrafael <rvs.contato@gmail.com>
2017-12-10 21:24:28 -0200

    provider

---
+++ src/providers/eighth-games/eighth-games.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the EighthGamesProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class EighthGamesProvider {
+
+  public eighthGames : any;
+  public apiUrl : string = "https://us-central1-profeta-da-bola.cloudfunctions.net/helloWorld";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello EighthGamesProvider Provider');
+  }
+
+  public load() {
+
+    console.log('Carregando oitavas de finais');
+    if (this.eighthGames) {
+      return Promise.resolve(this.eighthGames);
+    }
+
+    //var paramHeader = { HttpHeaders`x: "Access-Control-Allow-Origin:*" } ;
+
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl)
+      .subscribe(data => {
+        this.eighthGames = data;
+        this.eighthGames  = this.eighthGames.results;
+        resolve(this.eighthGames);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+}
