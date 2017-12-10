b8696ef045b68b3b0fce52b383cc54de8ae7cd02
rvsrafael <rvs.contato@gmail.com>
2017-12-10 21:54:31 -0200

    game request

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GameProvider } from '../../providers/game/game';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public games: any;
 
+  constructor(public navCtrl: NavController,
+                      public gameProvider: GameProvider)
+  {
+      this.loadGames();
+
+  }
+
+  loadGames() {
+    this.gameProvider.load()
+    .then(data => {
+      this.games = data;
+    });
   }
 
 }
